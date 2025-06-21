89574697fb1ada58302d57648bb6c7f19fcbcdfc
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2025-06-22 00:48:13 +0800

    set up

---
+++ Frontend/frag/src/main.tsx
@@ -0,0 +1,29 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import "./index.css";
+
+import { routeTree } from "./routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+// Create a new router instance
+const router = createRouter({ routeTree, context: { queryClient } });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
